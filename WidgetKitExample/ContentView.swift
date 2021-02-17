d3157208cedead8707e60345c9ef1421e5ba9484
tigi44 <onlytg@naver.com>
2021-02-17 14:46:20 +0900

    Initial Commit

---
+++ WidgetKitExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WidgetKitExample
+//
+//  Created by tigi KIM on 2021/02/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WidgetKitExample/WidgetKitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetKitExampleApp.swift
+//  WidgetKitExample
+//
+//  Created by tigi KIM on 2021/02/17.
+//
+
+import SwiftUI
+
+@main
+struct WidgetKitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
